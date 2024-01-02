6763e11dff89705b6ef68f886f279a7fdbaa33bf
elemen <124027876+elemen1069@users.noreply.github.com>
2024-01-02 23:43:42 +0900

    radius api add

---
+++ radius.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ * @param {{x: number, y: number, z: number}} location1 
+ * @param {{x: number, y: number, z: number}} location2 
+ * 
+ * Get radius in two location
+ * 두 좌표에 반지름을 가져옵니다.
+ * 
+ * @returns {result | false}
+ */
+function getradius(location1 = undefined, location2 = undefined) {
+    if (typeof location1 !== "object") {Error("location1 is not object"); return false;};
+    
+    if (typeof location2 !== "object") {Error("location2 is not object"); return false;};
+
+
+    const X = Math.round(location2.x) - Math.round(location1.x);
+    const Y = Math.round(location2.y) - Math.round(location1.y);
+    const Z = Math.round(location2.z) - Math.round(location1.z);
+
+    const result = Math.sqrt(Math.round(X ** 2 + Y ** 2 + Z ** 2));
+    return result;
+}
